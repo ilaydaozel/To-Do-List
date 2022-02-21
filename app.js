7d8b6044f7eb50937b9fe20ac1aacebf55020569
ilaydaozel <ilaydaoozel@gmail.com>
2022-02-21 22:40:25 +0300

    add new list item to the to-do-list

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 const express= require("express");
 const bodyParser= require("body-parser");
 const app=express();
+var items = ["Cook Food"];
 
 app.set('view engine', "ejs");
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", function(req,res){
 
   var today = new Date();
-  var currentDay = today.getDay();
-  var day="";
-  const dayArr = ["Sunday", "Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday"];
 
-  if(currentDay === 6 || currentDay===0){
-    day="Weekend";
-  }else{
-    day="Weekday";
-  }
-  res.render('list', {kindOfDay: dayArr[Number(currentDay)]});
+  var options ={
+    weekday: "long",
+    day:"numeric",
+    month: "long"
+  };
+  var day= today.toLocaleDateString("en-US", options);
+  res.render('list', {kindOfDay: day, newListItems: items});
+
   res.send();
 });
 
+app.post("/", function(req, res){
+  item = req.body.newLine;
+  items.push(item);
+  res.redirect("/"); //redirects to the homeroute
+})
 
 app.listen(3000, function(){
   console.log("server started on port 3000");
