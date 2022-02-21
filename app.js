db89a47b07c8f188e895220d1c61d1c50d2f9e95
ilaydaozel <ilaydaoozel@gmail.com>
2022-02-22 01:58:31 +0300

    add css file

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express= require("express");
 const bodyParser= require("body-parser");
 const app=express();
-var items = ["Cook Food"];
+var items = ["Cook Food", "Wash Dishes"];
 
 app.set('view engine', "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 app.get("/", function(req,res){
 
   var today = new Date();
