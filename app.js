790381ac199f80fcfc2fd20a55492a15bbe1e1e5
ilaydaozel <ilaydaoozel@gmail.com>
2022-02-27 21:50:10 +0300

    add mongo database

--- app.js
+++ app.js
@@ -1,19 +1,61 @@
 const express= require("express");
 const bodyParser= require("body-parser");
+const mongoose = require("mongoose");
 const date=require(__dirname + "/date.js");
 
 const app=express();
-const items = ["Cook Food", "Wash Dishes"];
-const workItems = [];
+// const items = ["Cook Food", "Wash Dishes"];
+// const workItems = [];
 
 app.set('view engine', "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/todolistDB")
+
+const itemsSchema = {
+  name: String
+};
+const Item = mongoose.model("Item", itemsSchema);
+const item1 = new Item({
+  name: "Welcome to your to do list"
+});
+const item2 = new Item({
+  name: "Hit the + button to add a new item"
+});
+const item3 = new Item({
+  name: "<-- Hit this to delete an item "
+});
+const defaultItems=[item1, item2, item3];
+
+
+
+
 app.get("/", function(req,res){
-  const day = date.getDay();
-  res.render('list', {listTitle: day, newListItems: items});
-  res.send();
+  Item.find({},function(err, foundItems){
+      if(err){
+        console.log(err);
+      }
+      else{
+        if(foundItems.length === 0){
+          //INSERT DEFAULT ELEMENTS
+          Item.insertMany(defaultItems, function(err){
+            if(err){
+              console.log(err);
+            }
+            else{
+              console.log("You inserted the items to DB");
+            }
+          });
+          res.redirect("/");
+        }
+        else{
+          res.render('list', {listTitle: "Today", newListItems: foundItems});
+        }
+
+      }
+  });
+  //res.send();
 });
 
 app.post("/", function(req, res){
